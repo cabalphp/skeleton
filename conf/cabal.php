29d72bfd6df1d41ffd3f2fe9fbe64c63c2b7111a
CabalPHP <cabalphp@outlook.com>
2018-06-13 23:13:19 +0800

    缓存配置

--- conf/cabal.php
+++ conf/cabal.php
@@ -4,5 +4,14 @@ return [
     'host' => '0.0.0.0',
     'port' => '9501',
 
+    'cache' => [
+        'default' => 'redis',
+        'redis' => [
+            'host' => '127.0.0.1',
+            'port' => '6379',
+            'auth' => '123456',
+        ],
+    ],
+
     'swoole' => [],
 ];
